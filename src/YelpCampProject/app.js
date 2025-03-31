6f042fc3de68ff4bf15d6fba6b029f629b63683d
Lee-sung-il <wj132612@naver.com>
2025-03-31 23:07:06 +0900

    💄 Layouts를 위한 새로운 EJS 툴
    ejs-mate를 설치하여 layout 적용 해보기

--- src/YelpCampProject/app.js
+++ src/YelpCampProject/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const path = require('path');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -18,6 +19,7 @@ db.once('open', function(){
 })
 
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
