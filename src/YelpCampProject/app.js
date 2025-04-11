c0fc5f729066126e1cdeca1dcbdbf689c0de0e65
Lee-sung-il <wj132612@naver.com>
2025-04-11 22:28:04 +0900

    💄 이미지 업데이트및 리뷰페이지 추가
    50개의 덤미 데이터 생성 및 리뷰데이터를 추가로 캠프그라운드에 리뷰모델 추가하여 관리 하는 기능

--- src/YelpCampProject/app.js
+++ src/YelpCampProject/app.js
@@ -8,7 +8,8 @@ const Campground = require('./models/campground');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/expressError');
 const err = require('./utils/ExpressError');
-const { campgroundSchema } = require('./schemas.js');
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
+const Review = require('./models/review');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
@@ -29,6 +30,16 @@ app.use(methodOverride('_method'));
 
 const validateCampground = (req, res, next) => {
   const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
   if (error) {
     const msg = error.details.map((el) => el.message).join(',');
     throw new ExpressError(msg, 400);
@@ -58,7 +69,6 @@ app.post(
   validateCampground,
   catchAsync(async (req, res, next) => {
     // if (!req.body.campground) throw new ExpressError('Invalid Campground Data',400);
-
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -68,7 +78,10 @@ app.post(
 app.get(
   '/campgrounds/:id',
   catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate(
+      'reviews',
+    );
+    // console.log(campground);
     res.render('campgrounds/show', { campground });
   }),
 );
@@ -102,6 +115,29 @@ app.delete(
   }),
 );
 
+app.post(
+  '/campgrounds/:id/reviews',
+  validateReview,
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+  }),
+);
+
+app.delete(
+  '/campgrounds/:id/reviews/:reviewId',
+  catchAsync(async (req, res) => {
+      const { id,reviewId } = req.params;
+      await Campground.findByIdAndUpdate(id,{$pull: {reviews: reviewId}});
+      await Review.findByIdAndUpdate(reviewId);
+      res.redirect(`/campgrounds/${id}`);
+  }),
+);
+
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page Not Found', 404));
 });
@@ -117,3 +153,5 @@ app.use((err, req, res, next) => {
 app.listen(3000, () => {
   console.log('Serving on port 3000');
 });
+
+//review
