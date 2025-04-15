9cf153777b65e8924fc368d650fe0be672e65103
Lee-sung-il <wj132612@naver.com>
2025-04-15 21:59:45 +0900

    💄 캠프그라운드 권한 부여
    편집 삭제 표시 및 숨기고 권한을 부여 받은 미들웨어 추가해보고 허가 리뷰 권한 리뷰 더 알아보기

--- src/YelpCampProject/routes/campgrounds.js
+++ src/YelpCampProject/routes/campgrounds.js
@@ -3,19 +3,7 @@ const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const router = express.Router();
 const ExpressError = require('../utils/expressError');
-const { campgroundSchema } = require('../schemas');
-const {isLoggedIn} = require('../middleware');
-
-
-const validateCampground = (req, res, next) => {
-  const { error } = campgroundSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(',');
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
 router.get(
   '/',
@@ -25,7 +13,7 @@ router.get(
   }),
 );
 
-router.get('/new',isLoggedIn ,(req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
   res.render('campgrounds/new');
 });
 
@@ -35,6 +23,7 @@ router.post(
   catchAsync(async (req, res, next) => {
     // if (!req.body.campground) throw new ExpressError('Invalid Campground Data',400);
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash('success', 'successfully made a new campground!');
     res.redirect(`/campgrounds/${campground._id}`);
@@ -43,35 +32,43 @@ router.post(
 
 router.get(
   '/:id',
-    catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate(
-      'reviews',
-    );
-    // console.log(campground);
-      if (!campground) {
-          req.flash('error', 'Cannot find that campground!');
-          return  res.redirect(`/campgrounds`);
-      }
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+      .populate({
+          path:'reviews',
+          populate: {
+              path: 'author'
+          }
+      })
+      .populate('author');
+    console.log(campground);
+    if (!campground) {
+      req.flash('error', 'Cannot find that campground!');
+      return res.redirect(`/campgrounds`);
+    }
     res.render('campgrounds/show', { campground });
   }),
 );
 
 router.get(
   '/:id/edit',
-  isLoggedIn ,
+  isLoggedIn,
+  isAuthor,
   catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-      if (!campground) {
-          req.flash('error', 'Cannot find that campground!');
-          return  res.redirect(`/campgrounds`);
-      }
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    if (!campground) {
+      req.flash('error', 'Cannot find that campground!');
+      return res.redirect(`/campgrounds`);
+    }
     res.render('campgrounds/edit', { campground });
   }),
 );
 
 router.put(
   '/:id',
-  isLoggedIn ,
+  isLoggedIn,
+  isAuthor,
   validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params;
@@ -85,11 +82,12 @@ router.put(
 
 router.delete(
   '/:id',
-  isLoggedIn ,
+  isLoggedIn,
+  isAuthor,
   catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
-      req.flash('success', 'Successfully deleted campground!');
+    req.flash('success', 'Successfully deleted campground!');
     res.redirect(`/campgrounds`);
   }),
 );
