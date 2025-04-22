0045a5768127f343299c00bdaef75e81d2599707
Lee-sung-il <wj132612@naver.com>
2025-04-22 16:37:48 +0900

    로그인 코드 다시 작성

--- src/YelpCampProject/routes/users.js
+++ src/YelpCampProject/routes/users.js
@@ -12,15 +12,27 @@ router
   .post(catchAsync(users.register));
 
 router.route('/login')
-.get(users.renderLogin)
-.post(
-    storeReturnTo,
-    passport.authenticate('local', {
-        failureFlash: true,
-        failureRedirect: '/login',
-    }),
-    users.login,
-);
+  .get(users.renderLogin)
+  .post(storeReturnTo, (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        req.flash('error', 'Invalid username or password');
+        return res.redirect('/login');
+      }
+      req.logIn(user, (err) => {
+        if (err) {
+          return next(err);
+        }
+        req.flash('success', 'Welcome back!');
+        const redirectUrl = req.session.returnTo || '/campgrounds';
+        delete req.session.returnTo;
+        res.redirect(redirectUrl);
+      });
+    })(req, res, next);
+  });
 
 // router.get('/logout', (req, res) => {
 //     req.logout();
